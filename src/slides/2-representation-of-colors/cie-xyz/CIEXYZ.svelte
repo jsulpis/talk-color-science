5e7008e16b9b5a53283d9eca380be6dff15969f2
Julien Sulpis <julien.sulpis@gmail.com>
2024-05-10 20:17:36 +0200

    feat: add slides for XYZ space and xy diagram

---
+++ src/slides/2-representation-of-colors/cie-xyz/CIEXYZ.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+	import { useGlslCanvas } from "../../../webgl/renderer";
+	import fragment from "./cie-xyz.frag";
+	import ciexyz from "../assets/cie-xyz.png";
+	import { onMount } from "svelte";
+
+	let canvas: HTMLCanvasElement;
+
+	onMount(() => {
+		useGlslCanvas({ canvas, fragment });
+	});
+</script>
+
+<div class="container">
+	<canvas bind:this={canvas}></canvas>
+	<img src={ciexyz.src} alt="" />
+</div>
+
+<style>
+	.container {
+		position: relative;
+		width: min(70%, 1200px);
+		margin-inline: auto;
+		margin-top: -5%;
+	}
+	canvas {
+		width: 100%;
+		aspect-ratio: 1.5;
+	}
+	img {
+		position: absolute;
+		width: 130%;
+		left: -11.5%;
+		bottom: 1%;
+		max-width: none;
+		max-height: none;
+	}
+</style>
